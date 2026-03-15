1b09592e7ba7988f71f6f3404f316ff9f6ed037d
rensodalin <rensodalin@user.noreply.github.com>
2026-03-15 21:26:40 +0700

    update

--- w8-smallhomework/data/repositories/songs/song_repository_mock.dart
+++ w8-smallhomework/data/repositories/songs/song_repository_mock.dart
@@ -53,11 +53,15 @@ class SongRepositoryMock implements SongRepository {
   @override
   Future<Song?> fetchSongById(String id) async {
     // - Simulate a delay of 3 seconds.
+    await Future.delayed(const Duration(seconds: 3));
 
     // - After the delay : Find the song of given id in the list of songs and return it
 
     // - If not found : Throw an error with the message “no song found for id 25 in the database"
-
-    return Future.delayed(Duration.zero); // TO CHANGE !
+    try {
+      return _songs.firstWhere((song) => song.id == id);
+    } catch (_) {
+      throw Exception('no song with $id in the database');
+    }
   }
 }
--- w8-smallhomework/test/song_repository_test.dart
+++ w8-smallhomework/test/song_repository_test.dart
@@ -1,10 +1,40 @@
- 
+import 'package:w5/asyn/data/repositories/songs/song_repository_mock.dart';
+
 void main() async {
   //   Instantiate the  song_repository_mock
- 
+  final repo = SongRepositoryMock();
+
   // Test both the success and the failure of the post request
- 
-  // Handle the Future using 2 ways  (2 tests)
   // - Using then() with .catchError().
+
+  // Handle the Future using 2 ways  (2 tests)
+  repo
+      .fetchSongById('s1')
+      .then((song) {
+        print('Song received ${song?.title}');
+      })
+      .catchError((e) {
+        print('Exception: $e');
+      });
+  repo
+      .fetchSongById('s9')
+      .then((song) {
+        print('Song received ${song?.title}');
+      })
+      .catchError((e) {
+        print('Exception: $e');
+      });
   // - Using async/await with try/catch.
+  try {
+    final song = await repo.fetchSongById('s2');
+    print('Song received ${song?.title}');
+  } catch (e) {
+    print('Exception: $e');
+  }
+  try {
+    final song = await repo.fetchSongById('s9');
+    print('Song received ${song?.title}');
+  } catch (e) {
+    print('Exception: $e');
+  }
 }
